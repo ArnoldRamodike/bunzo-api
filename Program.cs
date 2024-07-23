3e84f7335eb0cdaa23cb1f4e630d5a2a4ac799b0
ArnoldRamodike <riccoarnold23@gmail.com>
2024-07-23 12:57:23 +0200

    The DB Context setup with MySLQ

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Bulky.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("AppConnectionString"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("AppConnectionString"))));
 
 var app = builder.Build();
 
