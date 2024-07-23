0ebe039f77d9981aabc93a4d3b9626bddffe0aff
ArnoldRamodike <riccoarnold23@gmail.com>
2024-07-23 15:04:33 +0200

    Seeeding data and model updates

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace Bulky.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DispayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DispayOrder = 2 },
+                new Category { Id = 3, Name = "Drama", DispayOrder = 3 }
+            );
+        }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace Bulky.Models
     {
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DispayOrder { get; set; }
     }
 }
